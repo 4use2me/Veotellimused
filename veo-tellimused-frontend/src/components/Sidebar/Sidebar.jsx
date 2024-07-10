b22fd2376abf3e5529153e24ca08cc6a4911bdca
WINDOWS-1DCGN01\Kasutaja <forusetome@gmail.com>
2024-07-10 18:21:56 +0300

    sidebarUuendus

--- veo-tellimused-frontend/src/components/Sidebar/Sidebar.jsx
+++ veo-tellimused-frontend/src/components/Sidebar/Sidebar.jsx
@@ -1,32 +1,53 @@
 import React, { useState } from 'react';
 import './Sidebar.css';
 
-const Sidebar = ({ onNewOrder, onSelectOrderList, onNewClient, onSelectClientList, onNewVendor, onSelectVendorList }) => {
+const Sidebar = ({ onNewOrder, onSelectOrderList, onNewClient, onSelectClientList, onNewVendor, onSelectVendorList, onSettings, onSelectQueries }) => {
     const [clientsMenuOpen, setClientsMenuOpen] = useState(false);
     const [vendorsMenuOpen, setVendorsMenuOpen] = useState(false);
     const [ordersMenuOpen, setOrdersMenuOpen] = useState(false);
+    const [companysMenuOpen, setCompanysMenuOpen] = useState(false);
+
+    const toggleCompanysMenu = () => {
+        setCompanysMenuOpen(!companysMenuOpen);
+        setClientsMenuOpen(false);
+        setVendorsMenuOpen(false);
+        setOrdersMenuOpen(false);
+    };
 
     const toggleClientsMenu = () => {
         setClientsMenuOpen(!clientsMenuOpen);
         setVendorsMenuOpen(false);
         setOrdersMenuOpen(false);
+        setCompanysMenuOpen(false);
     };
 
     const toggleVendorsMenu = () => {
         setVendorsMenuOpen(!vendorsMenuOpen);
         setClientsMenuOpen(false);
         setOrdersMenuOpen(false);
+        setCompanysMenuOpen(false);
     };
 
     const toggleOrdersMenu = () => {
         setOrdersMenuOpen(!ordersMenuOpen);
         setClientsMenuOpen(false);
         setVendorsMenuOpen(false);
+        setCompanysMenuOpen(false);
     };
 
     return (
         <aside className="sidebar">
             <h2>Navigatsioon</h2>
+            <button className="toggle-menu-button" onClick={toggleCompanysMenu}>
+                {companysMenuOpen ? 'Ettevõte' : 'Ettevõte'}
+            </button>
+            {companysMenuOpen && (
+                <ul className="menu-items">
+                    <li onClick={onSettings}>Seaded</li>
+                    <li onClick={onSelectQueries}>Päringud</li>
+                    {/* Lisa siia teised alammenüü elemendid */}
+                </ul>
+            )}
             <button className="toggle-menu-button" onClick={toggleClientsMenu}>
                 {clientsMenuOpen ? 'Kliendid' : 'Kliendid'}
             </button>
